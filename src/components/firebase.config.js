69da261ff83222ed597579b6af9d6d15710e8472
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-04-24 09:18:18 +0600

    connecting firebase to deploy the site

---
+++ src/components/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB62nqWWhuecUtGGmsdxEEp17vahOdT4ew",
+    authDomain: "kothay-61ccc.firebaseapp.com",
+    projectId: "kothay-61ccc",
+    storageBucket: "kothay-61ccc.appspot.com",
+    messagingSenderId: "817512516497",
+    appId: "1:817512516497:web:1a038628e1c538fe752830"
+};
+export default firebaseConfig;
